daa31fd556609b6552ef747f2a28b88f8ac627b9
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-12 22:52:04 +0100

    add functions to open and close panel to add new transaction, add functions to save info from panel

---
+++ app.js
@@ -0,0 +1,51 @@
+const incomeSection = document.querySelector(".income-area");
+const expensesSection = document.querySelector(".espenses-area");
+const availableMoney = document.querySelector(".available-money");
+const addTransactionPanel = document.querySelector(".add-transaction-panel");
+
+const nameInput = document.querySelector("#name");
+const amountInput = document.querySelector("#amount");
+const categorySelect = document.querySelector("#category");
+
+const addTransactionBtn = document.querySelector(".add-transaction");
+const saveBtn = document.querySelector(".save");
+const cancelBtn = document.querySelector(".cancel");
+const deleteBtn = document.querySelector(".delete");
+const deleteAllBtn = document.querySelector(".delete-all");
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon;
+let selectCategory;
+let moneyArr = [0];
+
+const showPanel = () => {
+  addTransactionPanel.style.display = "flex";
+};
+
+const closePanel = () => {
+  addTransactionPanel.style.display = "none";
+  clearInputs();
+};
+
+const checkForm = () => {
+  if (
+    nameInput.value !== "" &&
+    amountInput.value !== "" &&
+    categorySelect.value !== "none"
+  ) {
+    console.log("ok");
+  } else {
+    alert("Wypełnij wszystkie pola!");
+  }
+};
+
+const clearInputs = () => {
+  nameInput.value = "";
+  amountInput.value = "";
+  categorySelect.selectedIndex = 0;
+};
+
+addTransactionBtn.addEventListener("click", showPanel);
+cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", checkForm);
