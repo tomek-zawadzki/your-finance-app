bff4dfd9d95ed8776c30201f041eddb91ac26199
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-13 13:07:16 +0100

    add functions to change mode from light to dark

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const cancelBtn = document.querySelector(".cancel");
 const deleteBtn = document.querySelector(".delete");
 const deleteAllBtn = document.querySelector(".delete-all");
 
+const lightStyle = document.querySelector(".light");
+const darkStyle = document.querySelector(".dark");
+
 let root = document.documentElement;
 let ID = 0;
 let categoryIcon;
@@ -124,7 +127,21 @@ const deleteAllTransactions = () => {
   moneyArr = [0];
 };
 
+const changeStyleToLight = () => {
+  root.style.setProperty("--first-color", "#f9f9f9");
+  root.style.setProperty("--second-color", "#14161f");
+  root.style.setProperty("--border-color", "rgba(0, 0, 0, 0.2)");
+};
+
+const changeStyleToDark = () => {
+  root.style.setProperty("--first-color", "#14161f");
+  root.style.setProperty("--second-color", "#f9f9f9");
+  root.style.setProperty("--border-color", "rgba(255, 255, 255, 0.4)");
+};
+
 addTransactionBtn.addEventListener("click", showPanel);
 cancelBtn.addEventListener("click", closePanel);
 saveBtn.addEventListener("click", checkForm);
 deleteAllBtn.addEventListener("click", deleteAllTransactions);
+lightStyle.addEventListener("click", changeStyleToLight);
+darkStyle.addEventListener("click", changeStyleToDark);
